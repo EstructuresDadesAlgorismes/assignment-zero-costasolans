2aa301f3e302ffd998bba15a88a0902a0d12612f
Pau Costa Clariana <pcostac@tcm.edu>
2016-04-21 13:00:39 +0200

    intellIJ changes, ready to start working

--- src/p0_material/Criatura.java
+++ src/p0_material/Criatura.java
@@ -14,9 +14,9 @@ public class Criatura implements Comparable{
     
     public Criatura(String nom, int edat, int sexe) {
         if (edat<MIN_EDAT || edat>MAX_EDAT)
-            throw new IllegalArgumentException("edat no vàlida: "+edat);
+            throw new IllegalArgumentException("edat no v?lida: "+edat);
         if (sexe!=NEN && sexe!=NENA)
-            throw new IllegalArgumentException("sexe no vàlid: "+sexe);
+            throw new IllegalArgumentException("sexe no v?lid: "+sexe);
         
         this.nom = nom;
         this.edat = edat;
@@ -27,26 +27,26 @@ public class Criatura implements Comparable{
     public int getEdat() {return this.edat;}
     public int getSexe() {return this.sexe;}
     
-    // redefinició del mètode toString heretat d'Object'
+    // redefinici? del m?tode toString heretat d'Object'
     public String toString () {
         String resultat;
-        resultat = "Criatura de nom: "+nom+", té "+edat+" anys. ";
+        resultat = "Criatura de nom: "+nom+", t? "+edat+" anys. ";
         if (sexe==NEN) resultat = resultat+"Sexe: nen";
         else resultat = resultat +"Sexe: nena";
         return resultat;
     }
     
-    // implementació de la interfície Comparable
+    // implementaci? de la interf?cie Comparable
     public int compareTo(Object o) {
-        // comparació basada en l'ordre lexicogràfic sense distingir majúsucules 
-        // de minúscules.
+        // comparaci? basada en l'ordre lexicogr?fic sense distingir maj?sucules 
+        // de min?scules.
         Criatura altra = (Criatura)o;
         return this.nom.compareToIgnoreCase(altra.nom);
     }
     
-    // redefinició del mètode equals heretat d'object.
+    // redefinici? del m?tode equals heretat d'object.
     public boolean equals (Object o) {
-        // redefinició compatible amb compareTo
+        // redefinici? compatible amb compareTo
         try {
             return this.compareTo(o)==0;
         }
